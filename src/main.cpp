7ea021599510735682054a7ea2611abc7a4f1696
ilcors-byte <c.corsetti.01@gmail.com>
2024-04-24 14:12:20 +0200

    fix: use 'Log` with `DEBUG` feature instead of `Serial`

--- src/main.cpp
+++ src/main.cpp
@@ -100,11 +100,11 @@ void sendMessage(void)
 	std::list<uint32_t> nodes = mesh.getNodeList(false);
 
 	if (mbus.bridgeSerialToMesh() == true) {
-		// Serial.printf("Dati da inviare formato stringa:\n");
-		// for (int i = 0; i < mbus.txlen; i++) {
-		// 	Serial.printf("%02X ", mbus.txSerial[i]);
-		// }
-		// Serial.printf("\n");
+		Log(DEBUG, "Dati da inviare formato stringa:\n");
+		for (int i = 0; i < mbus.txlen; i++) {
+			Log(DEBUG, "%02X ", mbus.txSerial[i]);
+		}
+		Log(DEBUG, "\n");
 
 		uint8_t buffer[100];
 		memcpy(buffer, mbus.txSerial, mbus.txlen);
@@ -117,71 +117,59 @@ void sendMessage(void)
 		}
 		String json;
 		serializeJson(meshMsg, json);
-		// Serial.println("dati json:");
-		// Serial.println(json);
 
-		// npkg_tx++;
-		// timeTx = millis();
-		// Serial.printf("Pkt inviato su mesh n. %d\n", npkg_tx);
-		// Serial.println("==========================================");
-		// Serial.println();
+		Log(DEBUG, "Dati json: %s\n", json.c_str());
+
+		npkg_tx++;
+		timeTx = millis();
+
+		Log(DEBUG, "Pkt inviato su mesh n. %d\n", npkg_tx);
+		Log(DEBUG, "==========================================\n");
+		Log(DEBUG, "\n");
 
 		mesh.sendBroadcast(json);
-		// Serial.printf("\n");
 	}
-
-	// taskSendReady.setInterval(TASK_SECOND * 5);
 
 	taskSendReady.setInterval(TASK_SEND);
 }
 /********************************************************************************/
 void MeshToSerialCallback(uint32_t from, String &msg)
 {
-	// uint16_t len;
 	String mesh_data;
-	//String copy = msg;
 
-	// Serial.printf("lunghezza dati rx Mesh: %d\n", msg.length());
-
-	// Serial.printf("Dati ricevuti:\n");
-	// for (int i = 0; i < msg.length(); i++) {
-	// 	Serial.printf("%02X ", msg.c_str()[i]);
-	// }
+	Log(DEBUG, "Len RX: %d\n", msg.length());
+	Log(DEBUG, "Dati RX: ");
+	for (int i = 0; i < msg.length(); i++) {
+		Log(DEBUG, "%02X ", msg.c_str()[i]);
+	}
+	Log(DEBUG, "\n");
 
 	DynamicJsonDocument doc(1024 + msg.length());
 	DeserializationError error = deserializeJson(doc, msg);
 	if (error) {
-		Serial.printf("DeserializationError\n");
+		Log(DEBUG, "DeserializationError\n");
 		return;
 	}
-	// Serial.printf("\n");
 
 	JsonArray rxData = doc.as<JsonArray>();
 	mbus.rxlen = rxData.size();
 
-	// Serial.println("Dati deserializzati in Array:");
+	Log(DEBUG, "Dati deserializzati in Array: ");
 	uint8_t k = 0;
 	for (JsonVariant v : rxData) {
-		// Serial.println(v.as<String>());
+		Log(DEBUG, "%d ", v.as<String>());
 		mbus.rxSerial[k] = v.as<String>().toInt();
 		k++;
 	}
 
-	// Serial.printf("\n");
-
-	Serial.printf("lunghezza dati in rxSerial: %d\n", mbus.rxlen);
-
-	// Serial.printf("\n");
 	mbus.bridgeMeshToSerial();
 }
 
 void newConnectionCallback(uint32_t nodeId)
 {
-	Log(COMMUNICATION, "Trovato nuovo nodo: %u\n", nodeId);
-	// Serial.println("Trovato nuovo nodo!!");
+	Log(DEBUG, "Trovato nuovo nodo: %u\n", nodeId);
 
 	std::list<uint32_t> nodes = mesh.getNodeList(false);
-	// Serial.printf("Pronti a ricevere dati n. %d\n", npkg);
 
 	mbus.mesh_map.push_back(nodeId);
 }
@@ -190,7 +178,6 @@ void changedConnectionCallback()
 {
 	Serial.printf("Changed connections\n");
 
-	// nodes = 0;
 	mesh.getNodeList();
 }
 
@@ -211,11 +198,12 @@ void setup()
 	pinMode(SCLK, INPUT);
 
 	//-- Mesh Init --------------------------------------------------------//
+
+	// Da usare per abilitare le funzioni di DEBUG
 	//mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
-	// mesh.setDebugMsgTypes( ERROR | STARTUP );  // set before init() so that you can see startup messages
-	// mesh.setDebugMsgTypes(ERROR | STARTUP | COMMUNICATION | APPLICATION);
+	// mesh.setDebugMsgTypes(DEBUG);
 	mesh.init(MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT);
-	// mesh.init(MESH_PREFIX, MESH_PASSWORD, MESH_PORT);
+
 	mesh.onNewConnection(&newConnectionCallback);
 	mesh.onChangedConnections(&changedConnectionCallback);
 	mesh.onNodeTimeAdjusted(&nodeTimeAdjustedCallback);
@@ -233,32 +221,5 @@ void loop()
 {
 	// it will run the user scheduler as well
 	mesh.update();
-
-	// Verifico se ci sono dei dati da inviare
-	// if (mbus.bridgeSerialToMesh() == true) {
-	// 	// Serial.printf("Dati da inviare formato stringa:\n");
-	// 	// for (int i = 0; i < mbus.txlen; i++) {
-	// 	// 	Serial.printf("%02X ", mbus.txSerial[i]);
-	// 	// }
-	// 	// Serial.printf("\n");
-
-	// 	uint8_t buffer[100];
-	// 	memcpy(buffer, mbus.txSerial, mbus.txlen);
-
-	// 	DynamicJsonDocument jsonBuffer(1024);
-	// 	JsonArray meshMsg = jsonBuffer.to<JsonArray>();
-
-	// 	for (uint8_t i = 0; i < mbus.txlen; i++) {
-	// 		meshMsg.add(buffer[i]);
-	// 	}
-	// 	String json;
-	// 	serializeJson(meshMsg, json);
-	// 	// Serial.println("dati json:");
-	// 	// Serial.println(json);
-
-	// 	mesh.sendBroadcast(json);
-	// 	// Serial.printf("\n");
-	// }
-
 	mbus.loop();
 }
